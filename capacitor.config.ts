43d205be8e7cba1f8f1918859e795304de949ed6
Hector Morales <hemoralesg2003@gmail.com>
2024-09-25 21:03:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Api-one-piece',
+  webDir: 'www'
+};
+
+export default config;
